064a905a6fbc5aaced4afd508aa9edf5520b61b6
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-06-26 18:03:53 -0300

    feat: Home finalizada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
+import Head from "next/head";
+import { ContentContainer, Hero } from "./index.style";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <ContentContainer>
+        <Hero>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the puclications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </Hero>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </ContentContainer>
+    </>
+  );
 }
